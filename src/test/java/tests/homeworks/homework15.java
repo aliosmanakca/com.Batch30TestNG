454cca9de60d2e3fff172a5df3efff71c22112d7
aliosmanakca <akcaaliosman@gmail.com>
2021-10-19 12:02:38 -0400

    .

--- src/test/java/tests/homeworks/homework15.java
+++ src/test/java/tests/homeworks/homework15.java
@@ -1,6 +1,16 @@
 package tests.homeworks;
 
-public class homework15 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class homework15 extends TestBase {
     /*
     ~ Navigate to http://tutorialsninja.com/demo/index.php?route=common/home
     ~ click on Phones & PDAs
@@ -14,4 +24,37 @@ public class homework15 {
     getName
     compareTwoList
      */
+    @Test
+    public void test01(){
+        driver.navigate().to("http://tutorialsninja.com/demo/index.php?route=common/home");
+
+        //~ click on Phones & PDAs
+        driver.findElement(By.partialLinkText("Phones & PDAs")).click();
+
+        //~ get the brandName of phones
+        List<String> brands = new ArrayList<>();
+        List<WebElement> brandsWeb = driver.findElements(By.tagName("h4"));
+        brandsWeb.stream().map(t->t.getText()).forEach(t->brands.add(t));
+
+        //~ click on add to button for all elements
+        List<WebElement> addToCart = driver.findElements(By.xpath("//span[.='Add to Cart']"));
+        addToCart.stream().forEach(t->t.click());
+
+        //~ click on black total added cart button
+        driver.findElement(By.xpath("//button[@class='btn btn-inverse btn-block btn-lg dropdown-toggle']")).click();
+
+        //~ get the names of list from the cart
+        List<WebElement> cartElements = driver.findElements(By.xpath("//td[@class='text-left']"));
+        List<String> stringCartElements = new ArrayList<>();
+        cartElements.stream().map(t->t.getText()).forEach(t->stringCartElements.add(t));
+
+        //~ compare the names from displaying list and cart list
+        Collections.sort(stringCartElements);
+        Collections.sort(brands);
+
+        System.out.println(brands);
+        System.out.println(stringCartElements);
+
+        Assert.assertEquals(brands,stringCartElements,"not same");
+    }
 }
